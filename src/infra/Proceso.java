78d940281efe4b3651d1901fd0a725e911ae6279
jhonson <jhonson1195@gmail.com>
2016-04-25 17:21:10 -0600

    Estados de proceso

---
+++ src/infra/Proceso.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infra;
+
+/**
+ *
+ * @author jhonson
+ */
+public class Proceso {
+    int ID;
+    String Estado;
+    int Dato;
+    int Duracion;
+    int sumatoria;
+    int tiemporestante;
+    int Resultado;
+    boolean Bloqueo;
+}
